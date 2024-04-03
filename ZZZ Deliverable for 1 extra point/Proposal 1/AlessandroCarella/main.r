c542a53a3fa7b500081df7602861bad942ae4f80
Alessandro Carella <79840231+AlessandroCarella@users.noreply.github.com>
2024-04-03 17:02:45 +0200

    cleaning

--- ZZZ Deliverable for 1 extra point/Proposal 1/AlessandroCarella/main.r
+++ ZZZ Deliverable for 1 extra point/Proposal 1/AlessandroCarella/main.r
@@ -15,32 +15,20 @@ query = formulate_query (
     #or sentences that needs to match in the query result
     "generative ai", 
     c(
-        "", #tags (strings separated by spaces)
-        "", #ignore tags (strings separated by spaces)
-        "", #user (integer as a string or "me" for the user (have to be logged in stackoverflow))
-        "", #min score (integer string)
-        "", #min answer (integer string)
-        "", #min view (integer string)
-        "", #code (string with code)
-        "", #closed ("yes" or "no")
-        "", #duplicated ("yes" or "no")
-        "", #is question ("yes" or "no")
-        ""
-        
         #for all of the following there is an example usage, 
         #you can always leave everything empty ("")
         #example
-        # "python opencv windows", #tags (strings separated by spaces)
-        # "macos linux", #ignore tags (strings separated by spaces)
-        # "1234 || me", #user (integer as a string or "me" for the user (have to be logged in stackoverflow))
-        # "1000", #min score (integer string)
-        # "10", #min answer (integer string)
-        # "2000", #min view (integer string)
-        # "print (\"hello\")", #code (string with code)
-        # "yes || no", #closed ("yes" or "no")
-        # "yes || no", #duplicated ("yes" or "no")
-        # "yes || no", #is question ("yes" or "no")
-        # "yes || no" #is answer ("yes" or "no")
+        "python opencv windows", #tags (strings separated by spaces)
+        "macos linux", #ignore tags (strings separated by spaces)
+        "1234 || me", #user (integer as a string or "me" for the user (have to be logged in stackoverflow))
+        "1000", #min score (integer string)
+        "10", #min answer (integer string)
+        "2000", #min view (integer string)
+        "print (\"hello\")", #code (string with code)
+        "yes || no", #closed ("yes" or "no")
+        "yes || no", #duplicated ("yes" or "no")
+        "yes || no", #is question ("yes" or "no")
+        "yes || no" #is answer ("yes" or "no")
     )
 )
 print ("Your query is")
