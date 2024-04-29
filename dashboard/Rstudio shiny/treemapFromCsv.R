80cc94c61d2ee5015d3e03c8fda78b41f061726a
Alessandro Carella <alessandro.carella.universita@gmail.com>
2024-04-29 18:04:02 +0200

    2 graphs in the same window

--- dashboard/Rstudio shiny/treemapFromCsv.R
+++ dashboard/Rstudio shiny/treemapFromCsv.R
@@ -5,7 +5,8 @@ library(treemap)
 ui <- fluidPage(
   titlePanel("Treemap Generator"),
   mainPanel(
-    plotOutput("treemapReddit")
+    plotOutput("treemapReddit"),
+    plotOutput("treemapTwitter")
   )
 )
 
@@ -15,6 +16,9 @@ server <- function(input, output, session) {
   dataReddit <- reactive({
     read.csv("data/question1RedditData.csv")
   })
+  dataTwitter <- reactive({
+    read.csv("data/question1TwitterData.csv")
+  })
   
   # Update column choices
   observe({
@@ -22,12 +26,21 @@ server <- function(input, output, session) {
       updateSelectInput(session, "companyReddit", choices = names(dataReddit()), selected = names(dataReddit())[1])
       updateSelectInput(session, "countReddit", choices = names(dataReddit()), selected = names(dataReddit())[2])
     }
+    if (!is.null(dataTwitter())) {
+      updateSelectInput(session, "companyTwitter", choices = names(dataTwitter()), selected = names(dataTwitter())[1])
+      updateSelectInput(session, "countTwitter", choices = names(dataTwitter()), selected = names(dataTwitter())[2])
+    }
   })
   
   # Render treemap
   output$treemapReddit <- renderPlot({
     req(dataReddit())
     tm <- treemap(dataReddit(), index = "company", vSize = "count")
+    print(tm)
+  })
+  output$treemapTwitter <- renderPlot({
+    req(dataTwitter())
+    tm <- treemap(dataTwitter(), index = "company", vSize = "count")
     print(tm)
   })
 }
