4d5933466e30e85f1633b5509c3c66253829910d
Alessandro Carella <alessandro.carella.universita@gmail.com>
2024-04-29 18:00:43 +0200

    renamed variable, still figuring this out, sorry for the commit

--- dashboard/Rstudio shiny/treemapFromCsv.R
+++ dashboard/Rstudio shiny/treemapFromCsv.R
@@ -5,50 +5,29 @@ library(treemap)
 ui <- fluidPage(
   titlePanel("Treemap Generator"),
   mainPanel(
-    plotOutput("treemap1"),
-    plotOutput("treemap2") # New plotOutput for the second treemap
+    plotOutput("treemapReddit")
   )
 )
 
 # Define server logic
 server <- function(input, output, session) {
-  # Read first CSV file
-  data1 <- reactive({
+  # Read CSV file
+  dataReddit <- reactive({
     read.csv("data/question1RedditData.csv")
   })
   
-  # Read second CSV file
-  data2 <- reactive({
-    read.csv("data/question1TwitterData") # Adjust the file path as needed
-  })
-  
-  # Update column choices for the first treemap
+  # Update column choices
   observe({
-    if (!is.null(data1())) {
-      updateSelectInput(session, "company", choices = names(data1()), selected = names(data1())[1])
-      updateSelectInput(session, "count", choices = names(data1()), selected = names(data1())[2])
+    if (!is.null(dataReddit())) {
+      updateSelectInput(session, "companyReddit", choices = names(dataReddit()), selected = names(dataReddit())[1])
+      updateSelectInput(session, "countReddit", choices = names(dataReddit()), selected = names(dataReddit())[2])
     }
   })
   
-  # Update column choices for the second treemap
-  observe({
-    if (!is.null(data2())) {
-      updateSelectInput(session, "company2", choices = names(data2()), selected = names(data2())[1])
-      updateSelectInput(session, "count2", choices = names(data2()), selected = names(data2())[2])
-    }
-  })
-  
-  # Render first treemap
-  output$treemap1 <- renderPlot({
-    req(data1())
-    tm <- treemap(data1(), index = "company", vSize = "count")
-    print(tm)
-  })
-  
-  # Render second treemap
-  output$treemap2 <- renderPlot({
-    req(data2())
-    tm <- treemap(data2(), index = "company2", vSize = "count2")
+  # Render treemap
+  output$treemapReddit <- renderPlot({
+    req(dataReddit())
+    tm <- treemap(dataReddit(), index = "company", vSize = "count")
     print(tm)
   })
 }
