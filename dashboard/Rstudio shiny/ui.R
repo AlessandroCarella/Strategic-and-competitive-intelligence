ff37670e9af5dc15207cf05d311a5aa1aa81b25b
Sara Hoxha <sarahoxha99@gmail.com>
2024-05-17 22:05:48 +0200

    fix typo

--- dashboard/Rstudio shiny/ui.R
+++ dashboard/Rstudio shiny/ui.R
@@ -389,7 +389,7 @@ public discussions (such as NVIDIA) and one can observe them in the treemap abov
             status = "success",
             width = 12,
             collapsible = T,
-            h4("Based on the data from Dev.To, it was observed that the prevailing opinions expressed  regarding remote working were predominantly positive. This indicates a favorable disposition towards emering digital nomadism work arrangements. The main sentiments described this arrangment as easy, new, and available. ")
+            h4("Based on the data from Dev.To, it was observed that the prevailing opinions expressed  regarding remote working were predominantly positive. This indicates a favorable disposition towards emerging digital nomadism work arrangements. The main sentiments described this arrangment as easy, new, and available. ")
           )
         )
       ),
